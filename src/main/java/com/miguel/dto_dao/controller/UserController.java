70b57aaebc689b5dafc27a5f2591cc6b23adac70
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-07-08 23:24:30 -0600

    uso de @Transient en la entidad

--- src/main/java/com/miguel/dto_dao/controller/UserController.java
+++ src/main/java/com/miguel/dto_dao/controller/UserController.java
@@ -1,28 +1,23 @@
 package com.miguel.dto_dao.controller;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.miguel.dto_dao.dto.UserDto;
-import com.miguel.dto_dao.entity.User;
 import com.miguel.dto_dao.service.IUserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserController {
 
-  @Autowired
-  private IUserService iUserService;
+  private final IUserService iUserService;
 
   @GetMapping()
   public List<UserDto> list() {
--- src/main/java/com/miguel/dto_dao/service/UserServiceImpl.java
+++ src/main/java/com/miguel/dto_dao/service/UserServiceImpl.java
@@ -1,8 +1,9 @@
 package com.miguel.dto_dao.service;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,16 +11,22 @@ import com.miguel.dto_dao.dto.UserDto;
 import com.miguel.dto_dao.entity.User;
 import com.miguel.dto_dao.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Service
 public class UserServiceImpl implements IUserService {
 
-  @Autowired
-  private UserRepository userRepository;
+  private final UserRepository userRepository;
 
   @Transactional(readOnly = true)
   @Override
   public List<UserDto> findAll() {
-    return this.userRepository.findAll().stream().map(user -> new UserDto(user.getName())).toList();
+    return this.userRepository.findAll().stream()
+        .map(user -> new UserDto(user.getName(), user.getName().concat(UUID.randomUUID().toString())))
+        .collect(Collectors.toList());
+    // return this.userRepository.findAll().stream().map(user -> new
+    // UserDto(user.getName())).toList();
   }
 
   @Transactional
